5ddeed28fdd61cb5dc0633ff93865eb0e9c7607d
Eduardo rojas Gomez <125102804+Eduardo1105rg@users.noreply.github.com>
2023-10-02 15:10:59 -0600

    Add files via upload

--- Proyecto_1_V1/Proyecto_1_V1_Eduardo_Rojas/logicadenegocios/Jugador.java
+++ Proyecto_1_V1/Proyecto_1_V1_Eduardo_Rojas/logicadenegocios/Jugador.java
@@ -1,35 +1,85 @@
 package logicadenegocios;
 
+import java.util.*;
 
 /**
  * Write a description of class Jugador here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author: Eduardo Rojas Gomez y Dylan Montiel
+ * @version: 10/2/2023
  */
-public class Jugador
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jugador
-     */
-    public Jugador()
-    {
-        // initialise instance variables
-        x = 0;
+public class Jugador {
+ 
+  private String nombre;
+  
+  private String correo;
+  
+  private String cedula;
+  
+  private ArrayList <Carton> cartonesAsignados;
+  
+    
+  /**
+   * Constructor for objects of class Jugador
+   */
+  public Jugador(String pNombre, String pCorreo, String Pcedula) {
+      
+    nombre = pNombre;
+    
+    correo = pCorreo;
+    
+    cedula = Pcedula;    
+      
+  }
+  
+  public String getNombre() {
+    
+    return this.nombre;
+      
+  }
+  
+  public String getCorreo() {
+    
+    return this.correo;
+      
+  }
+  
+  public String getCedula() {
+    
+    return this.cedula;
+      
+  }
+  
+  //public String get() {
+    
+    //return ;
+      
+  //}
+  
+  public void asignarCarton(Carton nuevoCarton) {
+    
+    cartonesAsignados.add(nuevoCarton);       
+    
+  }
+  
+  
+  public String toString() {
+    
+  String msg = "";
+  
+  msg = "Nombre: " + nombre + "\n";
+  
+  msg += "Correo: " + correo + "\n";
+  
+  msg += "Cedula: " + cedula + "\n";
+  
+  for (int i = 0; i < cartonesAsignados.size(); i++) {
+    
+    
     }
+  
+  return msg;  
+    
+  }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
